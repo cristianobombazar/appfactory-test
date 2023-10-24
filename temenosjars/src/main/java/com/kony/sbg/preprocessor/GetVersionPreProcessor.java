94f8df19d96eefb4213413a534e32f7405f2443a
cristianointegritas <61697734+cristianointegritas@users.noreply.github.com>
2023-10-24 10:35:04 +0100

    new java code v2

--- temenosjars/src/main/java/com/kony/sbg/preprocessor/GetVersionPreProcessor.java
+++ temenosjars/src/main/java/com/kony/sbg/preprocessor/GetVersionPreProcessor.java
@@ -8,7 +8,7 @@ import com.konylabs.middleware.dataobject.Result;
 public class GetVersionPreProcessor implements DataPostProcessor2 {
     @Override
     public Object execute(Result result, DataControllerRequest dataControllerRequest, DataControllerResponse dataControllerResponse) throws Exception {
-        result.addStringParam("appfactorytest-1.0.0.jar", "v1");
+        result.addStringParam("appfactorytest-1.0.0.jar", "v2");
         return result;
     }
 }
